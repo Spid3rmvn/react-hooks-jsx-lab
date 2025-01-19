34b240aaac941c7811d0840cd17551b437a38983
Godwin Mayodi <godwin.mayodi@student.moringaschool.com>
2025-01-20 02:02:39 +0300

    build(deps): update package-lock.json, About.js, Home.js and NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { name, city } from "../data/data.js";
 
-function NavBar() {
-  // update the JSX being returned!
-  return <nav>NavBar</nav>;
+function Home() {
+  return (
+    <div id="home">
+      <h1 style={{ color: "firebrick" }}>
+        {name} is a Web Developer from {city}
+      </h1>
+    </div>
+  );
 }
 
-export default NavBar;
+export default Home;
